f04d3a806a1e59c61eb5ca891b24e58aaced539f
Koray Ozkal <koryozkal@gmail.com>
2020-04-04 04:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ function sayHiToGrandma(string) {
 console.log('i can\'t hear you')
 return "I can't hear you!"
 }
+function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+      var lowerCase = string.toLowerCase();
+      return ('I can\'t hear you!');
+    }
+    else if (string === string.toUpperCase()){
+      var upperCase = 'YES INDEED!';
+      return ('YES INDEED!');
+      }
+      else{
+        var mixedCase = 'I love you, too.';
+        return(mixedCase);
+      }
+    }
